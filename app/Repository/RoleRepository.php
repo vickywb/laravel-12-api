43ae59084d62ba664af79076e7609c810f2d9b98
vicky wibisono <wibivicky@gmail.com>
2025-05-21 10:43:00 +0700

    create role repository

--- app/Repository/RoleRepository.php
+++ app/Repository/RoleRepository.php
@@ -11,4 +11,25 @@ class RoleRepository
     public function __construct(Role $role) {
         $this->role = $role;
     }
+
+    public function get($params = [])
+    {
+        $roles = $this->role
+            ->when(!empty($params['search']['name']), function($query) use ($params) {
+                return $query->where('name', 'LIKE', '%' . $params['search']['name'] . '%');
+            });
+            
+        if (!empty($params['page'])) {
+            return $roles->paginate($params['page']);
+        }
+
+        return $roles->get();
+    }
+
+    public function store(Role $role)
+    {
+        $role->save();
+
+        return $role;
+    }
 }
