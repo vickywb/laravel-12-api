b41c260dd418e3f6c5cc2735f369ea53c56d1ffb
vicky wibisono <wibivicky@gmail.com>
2025-05-24 08:13:21 +0700

    add product request store and update

---
+++ app/Http/Requests/ProductStoreRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:50|unique:products,name',
+            'price' => 'required|numeric',
+            'stock' => 'required|integer|min:1',
+            'description' => 'required|string|max:255',
+            'product_url'  => 'required|string|max:255'
+        ];
+    }
+}
