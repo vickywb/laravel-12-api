09bc55141244a8ea392c1e60ebd614a106c0c7f3
vicky wibisono <wibivicky@gmail.com>
2025-05-24 08:13:36 +0700

    add product resource and collection

---
+++ app/Http/Resources/ProductCollection.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ProductCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $products = $this->collection->transform(function ($product) use ($request) {
+            return [
+                'id' => $product->id,
+                'name' => $product->name,
+                'price' => $product->price,
+                'stock' => $product->stock,
+                'description' => $product->description,
+                'product_url' => $product->product_url,
+                'category' => $product->category->name
+            ];
+        });
+
+        return $products->toArray();
+    }
+}
