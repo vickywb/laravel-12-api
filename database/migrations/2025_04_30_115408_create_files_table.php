561d61c3a3abce9256ff86843711c62f41268648
vicky wibisono <wibivicky@gmail.com>
2025-05-28 21:47:10 +0700

    add column file_url on table files

--- database/migrations/2025_04_30_115408_create_files_table.php
+++ database/migrations/2025_04_30_115408_create_files_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name')->unique();
             $table->string('directory');
+            $table->string('file_url');
             $table->dateTime('upload_at');
             $table->timestamps();
         });
