8d221073a68504a1cf98c33038ebcfb75fafe9fa
vicky wibisono <wibivicky@gmail.com>
2025-05-28 21:46:48 +0700

    add relationship on file model

--- app/Models/File.php
+++ app/Models/File.php
@@ -3,11 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class File extends Model
 {
     protected $fillable = [
-        'name', 'directory', 'upload_at'
+        'name', 'directory', 'upload_at', 'file_url'
     ];
 
     protected function casts(): array
@@ -16,5 +17,21 @@ class File extends Model
             'created_at' => 'datetime',
             'upload_at' => 'datetime'
         ];
+    }
+
+    // Relationship
+    public function userProfiles(): HasMany
+    {
+        return $this->hasMany(UserProfile::class);
+    }
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    public function productFiles(): HasMany
+    {
+        return $this->hasMany(ProductFile::class);
     }
 }
