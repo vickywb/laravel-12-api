53c6059152e96e330988921a9ece06d08cbc3bbe
vicky wibisono <wibivicky@gmail.com>
2025-06-03 22:53:18 +0700

    add shcedule command to delete token expired on app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Console\Scheduling\Schedule;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->app->booted(function () {
+            $schedule = app(Schedule::class);
+            $schedule->command('token:delete-expired-tokens')->hourly();
+        });
     }
 }
