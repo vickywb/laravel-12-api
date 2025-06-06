65591cc3e9145f51ee2fc87c9f8bd1c350c350c9
vicky wibisono <wibivicky@gmail.com>
2025-06-06 19:39:33 +0700

    update routes api

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V1\Admin\CategoryController;
+use App\Http\Controllers\Api\V1\File\FileController;
 use App\Http\Controllers\Api\V1\Admin\ProductController;
 use App\Http\Controllers\Api\V1\Admin\RoleController;
 use App\Http\Controllers\Api\V1\Auth\AuthController;
@@ -8,13 +9,32 @@ use App\Http\Controllers\Api\V1\Auth\AuthController;
 
 Route::prefix('auth')
     ->controller(AuthController::class)
+    ->middleware('guest')
     ->group(function () {
         Route::post('login', 'login')->middleware('throttle-api:login,3,60'); // Limit 3 times delayed 1 minute
-        Route::post('register', 'register')->middleware('throttle-api:register,3,180'); // Limit 3 times delayed 3 minute
-        Route::post('logout', 'logout')->middleware('auth-api');
+        Route::post('register', 'register'); // Limit 3 times delayed 3 minute
+});
+
+
+Route::prefix('files')
+    ->controller(FileController::class)
+    ->group(function () {
+        Route::post('/upload', 'upload');
 });
 
 Route::middleware('auth-api')->group(function () {
+
+    Route::prefix('auth')
+        ->controller(AuthController::class)
+        ->group(function () {
+            Route::post('logout', 'logout');
+    });
+
+    Route::prefix('users')
+        ->controller(AuthController::class)
+        ->group(function () {
+            Route::patch('update-profiles', 'updateProfile');
+    });
 
     Route::prefix('admin')
         ->middleware('admin-api')
@@ -48,13 +68,6 @@ Route::middleware('auth-api')->group(function () {
                 Route::get('{product}/show', 'show');
                 Route::patch('{product}/update', 'update');
                 Route::delete('{product}/delete', 'destroy');
-
-                // Product Files
-                Route::get('{product}/product-files', 'fileIndex');
-                Route::post('{product}/product-files/store', 'fileStore');
-                Route::patch('{product}/product-files/update/{file}', 'fileUpdate');
-                Route::delete('{product}/product-files/delete/{file}', 'fileDestroy');
         });
     });
-    
 });
