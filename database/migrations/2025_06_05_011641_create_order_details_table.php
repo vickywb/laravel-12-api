40e201df0da6a3f3c615499c54c00c03340eb05d
vicky wibisono <wibivicky@gmail.com>
2025-06-08 01:30:50 +0700

    add order detail model, table and controller

---
+++ database/migrations/2025_06_05_011641_create_order_details_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->integer('quantity')->default(1); // Quantity of the product in the order
+            $table->decimal('unit_price', 16, 2); // Price of the product at the time of order
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_details');
+    }
+};
