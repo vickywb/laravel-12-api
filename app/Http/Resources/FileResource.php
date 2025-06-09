3bfe3cedb3e0c6d57702d9b6b5758905a0d94a7a
vicky wibisono <wibivicky@gmail.com>
2025-06-09 22:57:45 +0700

    update file resource

---
+++ app/Http/Resources/FileResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FileResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'file_url' => $this->file_url,
+            'upload_at' => $this->upload_at->format('d-m-Y H:i:s'),
+        ];
+    }
+}
