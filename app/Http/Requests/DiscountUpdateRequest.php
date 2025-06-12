eb0bed2cbaccac8d68c0356f6b7a71708cf8e7a7
vicky wibisono <wibivicky@gmail.com>
2025-06-12 23:52:25 +0700

    add global discount store and update request

---
+++ app/Http/Requests/DiscountUpdateRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DiscountUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'code' => 'required|string|max:255|unique:discounts,code,' . $this->discount->id,
+            'discount_type' => 'required|in:percentage,fixed',
+            'discount_amount' => 'required|numeric|min:0',
+            'start_at' => 'required|date_format:Y-m-d H:i:s',
+            'end_at' => 'required|date_format:Y-m-d H:i:s|after_or_equal:start_at',
+            'minimum_order_total' => 'nullable|numeric|min:0',
+        ];
+    }
+}
