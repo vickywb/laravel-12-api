e85ec02349228b35e35ab39f6e5aeff5ca30eceb
vicky wibisono <wibivicky@gmail.com>
2025-06-18 09:17:11 +0700

    add cart resource

---
+++ app/Http/Resources/CartResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CartResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'quantity' => $this->quantity,
+            'price_at_time' => (string) $this->price_at_time,
+            'product' => [
+                'id' => $this->product->id,
+                'name' => $this->product->name,
+                'price' => (string) $this->product->price,
+                'product_file' => new FileResource($this->product->productFile ?? null),
+                'product_discuont' => new ProductDiscountResource($this->product->activeDiscount ?? null)
+            ]
+        ];
+    }
+}
