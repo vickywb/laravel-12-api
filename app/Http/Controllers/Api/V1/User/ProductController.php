1ae12779942dbf1ea52531cb63dbd0c5e73c49d1
vicky wibisono <wibivicky@gmail.com>
2025-06-21 17:27:47 +0700

    add product controller index for user

---
+++ app/Http/Controllers/Api/V1/User/ProductController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\User;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use App\Helpers\ResponseApiHelper;
+use App\Http\Controllers\Controller;
+use App\Repository\ProductRepository;
+use App\Http\Resources\ProductCollection;
+use App\Http\Resources\ProductResource;
+
+class ProductController extends Controller
+{
+    private $productRepository;
+
+    public function __construct(ProductRepository $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
+    public function index()
+    {
+        $products = $this->productRepository->get([
+            'with' => ['category'],
+            'search' => [
+                'name' => request()->name,
+                'category_name' => request()->category_name,
+            ],
+            'page' => 5,
+        ]);
+
+        if ($products->isEmpty()) {
+            return ResponseApiHelper::error('Product is not found.', [], 404);
+        }
+
+        return ResponseApiHelper::success('Products retrieved successfully.', new ProductCollection($products));
+    }
+
+    public function show(Product $product)
+    {
+        return ResponseApiHelper::success('Product retrieved successfully.', new ProductResource($product));
+    }
+}
