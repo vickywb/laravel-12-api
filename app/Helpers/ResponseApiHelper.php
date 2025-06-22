8e63e82c4485728341f2a5a6f733941a20f2f92c
vicky wibisono <wibivicky@gmail.com>
2025-06-23 00:06:38 +0700

    update response api helper, add check data collection

--- app/Helpers/ResponseApiHelper.php
+++ app/Helpers/ResponseApiHelper.php
@@ -2,35 +2,49 @@
 
 namespace App\Helpers;
 
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
 class ResponseApiHelper
 {
     // This is reponse status when status is success
     public static function success(
         string $message,
-        array|object $data = [],
+        array|object|null $data = null,
         int $statusCode = 200,
         string $status = 'Success'
     )
     {
+        // check data is a resource collection
+        if ($data instanceof ResourceCollection) {
+            $dataArray = $data->response()->getData(true);
+            return response()->json([
+                'status'  => $status,
+                'message' => $message,
+                'data'    => $dataArray['data'],
+                'meta'    => $dataArray['meta'] ?? null,
+                'links'   => $dataArray['links'] ?? null,
+            ], $statusCode);
+        }
+        
         return response()->json([
-            'status' => $status,
-            'message' => $message,
-            'data' => $data
+            'status'    => $status,
+            'message'   => $message,
+            'data'      => $data
         ], $statusCode);
     }
 
     // This is reponse status when status is error
     public static function error(
         string $message,
-        array|object $data = [],
+        array|object|null $data = null,
         int $statusCode = 500,
         string $status = 'Errors'
     )
     {
         return response()->json([
-            'status' => $status,
-            'message' => $message,
-            'data' => $data
+            'status'    => $status,
+            'message'   => $message,
+            'data'      => $data
         ], $statusCode);
     }
 }
