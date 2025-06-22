5b61fbedf6be0ebf32120d173a7a9da50ae2f7ac
vicky wibisono <wibivicky@gmail.com>
2025-06-22 18:08:04 +0700

    add order detail resource

---
+++ app/Http/Resources/OrderDetailResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrderDetailResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'product_id' => $this->product_id,
+            'product_name' => $this->product->name, 
+            'quantity' => $this->quantity,
+            'unit_price' => $this->unit_price
+        ];
+    }
+}
