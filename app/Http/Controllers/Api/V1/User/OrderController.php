dfb768c5ae2b9a93984c1c74825cf92adb3382bf
vicky wibisono <wibivicky@gmail.com>
2025-06-28 21:40:15 +0700

    update order index and store on order controller

--- app/Http/Controllers/Api/V1/User/OrderController.php
+++ app/Http/Controllers/Api/V1/User/OrderController.php
@@ -2,46 +2,67 @@
 
 namespace App\Http\Controllers\Api\V1\User;
 
-use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Helpers\AuthHelper;
 use Illuminate\Http\Request;
+use App\Services\OrderService;
+use App\Helpers\ResponseApiHelper;
+use Illuminate\Support\Facades\DB;
+use App\Repository\OrderRepository;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\OrderResource;
 
 class OrderController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    private $orderRepository, $orderService;
+
+    public function __construct(
+        OrderRepository $orderRepository,
+        OrderService $orderService
+    ) 
+    {
+        $this->orderRepository = $orderRepository;
+        $this->orderService = $orderService;
+    }
+    
     public function index()
     {
-        //
+        $orders = $this->orderRepository->get([
+            'order' => 'created_at desc',
+            'search' => [
+                'status' => request()->order_status
+            ],
+            'page' => 5
+        ]);
+
+        return ResponseApiHelper::success('Order retrived successfully.', OrderResource::collection($orders));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $user = AuthHelper::getUserFromToken($request->bearerToken());
+
+        try {
+
+            $order = $this->orderService->createOrderFromCart($user->id);
+        
+        } catch (\Throwable $th) {
+
+            return ResponseApiHelper::error('An error occurred while proccess store order data. Please try again later.');
+        }
+
+        return ResponseApiHelper::success('Order has been created successfully.', new OrderResource($order));
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function show(Order $order)
     {
-        //
+        return ResponseApiHelper::success('Order retrived successfully.', new OrderResource($order));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         //
     }
-
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         //
