03d961771868401dd2066e9073fbd754c63233dc
vicky wibisono <wibivicky@gmail.com>
2025-07-01 22:13:54 +0700

    fixing typo on transaction detail table

--- database/migrations/2025_06_05_012141_create_transaction_details_table.php
+++ database/migrations/2025_06_05_012141_create_transaction_details_table.php
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('transaction_detais');
+        Schema::dropIfExists('transaction_details');
     }
 };
