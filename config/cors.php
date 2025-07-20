28eec22312c5dc706c1c28e6a1df3a7f2cad886d
vicky wibisono <wibivicky@gmail.com>
2025-07-20 23:50:24 +0700

    update cors config

---
+++ config/cors.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['http://localhost:5173'], // ini sesuaikan dengan port Vue kamu
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+];
