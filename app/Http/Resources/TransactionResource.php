3d94c6236c833eb01e9aa1deec54c8248c91f97e
vicky wibisono <wibivicky@gmail.com>
2025-07-21 23:23:52 +0700

    add transaction resource

---
+++ app/Http/Resources/TransactionResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TransactionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'invoice_number' => $this->invoice_number,
+            'transaction_status' => $this->transaction_status,
+            'payment_method' => $this->payment_method,
+            'total_price' => $this->total_price,
+            'bank' => $this->bank,
+            'paid_at' => $this->paid_at ? $this->paid_at->format('Y-m-d H:i:s') : null,
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at->format('Y-m-d H:i:s')
+        ];
+    }
+}
