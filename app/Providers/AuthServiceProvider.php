d31c91aa6312317104296205afbe1647db44d0b6
vicky wibisono <wibivicky@gmail.com>
2025-07-24 08:00:35 +0700

    add providers and auth service providers

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Order;
+use App\Models\Transaction;
+use App\Policies\OrderPolicy;
+use App\Policies\TransactionPolicy;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    protected $policies = [
+        Transaction::class => TransactionPolicy::class,
+        Order::class => OrderPolicy::class
+    ];
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $this->registerPolicies();
+    }
+}
