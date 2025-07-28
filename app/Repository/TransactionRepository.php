d61d0e6df1bc7f3039b1908c33977e02e6fc9ff6
vicky wibisono <wibivicky@gmail.com>
2025-07-29 06:02:55 +0700

    update transaction repository

--- app/Repository/TransactionRepository.php
+++ app/Repository/TransactionRepository.php
@@ -16,28 +16,40 @@ class TransactionRepository
     public function get($params = [])
     {
         $transactions = $this->transaction
+            ->when(!empty($params['user_id']), function ($query) use ($params) {
+                return $query->where('user_id', $params['user_id']);
+            })
             ->when(!empty($params['orders']), function ($query) use ($params) {
                 return $query->orderByRaw($params['orders']);
-            })->when(!empty($params['search']['status']), function ($query) use ($params) {
+            })
+            ->when(!empty($params['with']), function ($query) use ($params) {
+                return $query->with($params['with']);
+            })
+            ->when(!empty($params['search']['status']), function ($query) use ($params) {
                 return $query->where('transaction_status', 'like', '%' . $params['search']['status'] . '%');
             })->when(!empty($params['search']['invoice_number']), function ($query) use ($params) {
                 return $query->where('invoice_number', 'like', '%' . $params['search']['invoice_number'] . '%');
-            })->when(!empty($params['search']['payment_method']), function ($query) use ($params) {
-                return $query->where('payment_method', 'like', '%' . $params['search']['payment_method'] . '%');
-            })->when(!empty($params['search']['date_from']), function ($query) use ($params) {
+            })
+            ->when(!empty($params['search']['bank']), function ($query) use ($params) {
+                return $query->where('bank', 'like', '%' . $params['search']['bank'] . '%');
+            })
+            ->when(!empty($params['search']['date_from']), function ($query) use ($params) {
                 return $query->whereDate('created_at', '>=', $params['search']['date_from']);
-            })->when(!empty($params['search']['date_to']), function ($query) use ($params) {
+            })
+            ->when(!empty($params['search']['date_to']), function ($query) use ($params) {
                 return $query->whereDate('created_at', '<=', $params['search']['date_to']);
-            })->where(!empty($params['search']['total_min']), function ($query) use ($params) {
+            })
+            ->when(!empty($params['search']['total_min']), function ($query) use ($params) {
                 return $query->where('total_price', '>=', $params['search']['total_min']);
-            })->where(!empty($params['search']['total_max']), function ($query) use ($params) {
+            })
+            ->when(!empty($params['search']['total_max']), function ($query) use ($params) {
                 return $query->where('total_price', '<=', $params['search']['total_max']);
             });
 
         if (!empty($params['page'])) {
-            $transactions = $transactions->paginate($params['page']);
+            return $transactions->paginate($params['page']);
         }
 
-        return $transactions;
+        return $transactions->get();
     }
 }
