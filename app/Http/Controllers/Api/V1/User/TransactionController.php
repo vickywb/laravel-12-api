27efa2fe64531abffdecf9b2a71b65e89d70532e
vicky wibisono <wibivicky@gmail.com>
2025-07-28 20:41:25 +0700

    update transaction controller

--- app/Http/Controllers/Api/V1/User/TransactionController.php
+++ app/Http/Controllers/Api/V1/User/TransactionController.php
@@ -2,31 +2,59 @@
 
 namespace App\Http\Controllers\Api\V1\User;
 
-use App\Enums\PaymentStatus;
 use Midtrans\Snap;
 use Midtrans\Config;
 use App\Models\Order;
 use App\Helpers\AuthHelper;
 use App\Models\Transaction;
+use App\Enums\PaymentStatus;
+use App\Exceptions\ApiException;
 use Illuminate\Http\Request;
 use App\Helpers\LoggerHelper;
 use App\Models\TransactionDetail;
+use App\Services\MidtransService;
 use App\Helpers\ResponseApiHelper;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
-use App\Services\MidtransService;
+use App\Repository\TransactionRepository;
+use App\Http\Resources\TransactionResource;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class TransactionController extends Controller
 {
-    private $midtrans;
+    private $midtrans, $transactionRepository;
+    use AuthorizesRequests;
 
-    public function __construct(MidtransService $midtrans) {
+    public function __construct(
+        MidtransService $midtrans,
+        TransactionRepository $transactionRepository
+    ) {
         $this->midtrans = $midtrans;
+        $this->transactionRepository = $transactionRepository;
     }
 
     public function index()
     {
-        //
+        $user = AuthHelper::getUserFromToken(request()->bearerToken());
+
+        $transactions = $this->transactionRepository->get([
+            'orders' => 'created_at DESC',
+            'with' => ['transactionDetails.product', 'user.userProfile'],
+            'user_id' => $user->id,
+            'search' => [
+                'status' => request('status'),
+                'invoice_number' => request('invoice_number'),
+                'bank' => request('bank'),
+                'date_from' => request('date_from'),
+                'date_to' => request('date_to'),
+                'total_min' => request('total_min'),
+                'total_max' => request('total_max')
+            ],
+            'page' => 10
+        ]);
+
+        return ResponseApiHelper::success('Transaction data retrieved successfully.', TransactionResource::collection($transactions));
     }
 
     public function store(Request $request, Order $order)
@@ -38,7 +66,7 @@ class TransactionController extends Controller
             ->findOrFail($order->id);
         
         if ($order->final_price <= 0) {
-            throw new \Exception('Invalid final price.');
+            throw new ApiException('Invalid final price.');
         }
 
         $totalPrice = $order->final_price;
@@ -54,7 +82,7 @@ class TransactionController extends Controller
                 'invoice_number' => $invoiceNumber,
                 'payment_method' => 'midtrans',
                 'total_price' => $totalPrice,
-                'payment_status' => PaymentStatus::UNPAID
+                'transaction_status' => PaymentStatus::UNPAID->value
             ]);
 
             foreach ($order->orderDetails as $transactionDetail) {
@@ -142,5 +170,21 @@ class TransactionController extends Controller
             'invoice_url' => $invoiceUrl,
             'snap_token' => $snapToken['token'] ?? null
         ]);
+    }
+
+    public function show(Transaction $transaction)
+    {
+        $user = AuthHelper::getUserFromToken(request()->bearerToken());
+        auth()->loginUsingId($user->id); // convert to auth user from token
+
+        $transaction->load('transactionDetails.product');
+        
+        try {
+            $this->authorize('view', $transaction);
+        } catch (AuthorizationException $e) {
+            return ResponseApiHelper::error('You do not have permission to view this transaction.', [], 403);
+        }
+        
+        return ResponseApiHelper::success('Transaction data retrieved successfully.', new TransactionResource($transaction));
     }
 }
