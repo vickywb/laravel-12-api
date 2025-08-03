86bb8ef7d1bebf3ada64b90264374a1e667e30f0
vicky wibisono <wibivicky@gmail.com>
2025-08-03 20:12:57 +0700

    update home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Helpers\ResponseApiHelper;
+use App\Http\Resources\ProductCollection;
 use App\Repository\ProductRepository;
 use App\Http\Resources\ProductResource;
 
@@ -18,7 +19,20 @@ class HomeController extends Controller
 
     public function index()
     {
-        return ResponseApiHelper::success('Successfully Retrived Data.', ProductResource::collection(Product::latest()->take(8)->get()));
+        $newProducts = $this->productRepository->get([
+            'order' => 'created_at desc',
+            'page' => 8
+        ]);
+
+        $discountProducts = $this->productRepository->get([
+            'whereHas' => 'activeDiscount',
+            'with' => 'activeDiscount'
+        ]);
+
+        return ResponseApiHelper::success('Successfully Retrived Data.', [
+            'new_products' => ProductResource::collection($newProducts),
+            'discount_products' => ProductResource::collection($discountProducts)
+        ]);
     }
 
     public function show(string $id)
