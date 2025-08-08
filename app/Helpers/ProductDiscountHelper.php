dc128b8f9405276cf80af2acdba5ec2a8a0b15ad
vicky wibisono <wibivicky@gmail.com>
2025-08-08 20:49:57 +0700

    update product discount helper

--- app/Helpers/ProductDiscountHelper.php
+++ app/Helpers/ProductDiscountHelper.php
@@ -9,9 +9,13 @@ class ProductDiscountHelper
     public static function getPriceAtTime(Product $product): string
     {
         if ($product->activeDiscount) {
-            return bcsub($product->price, $product->activeDiscount->discount_price, 2); //2  decimal
+            if ($product->activeDiscount->discount_type === 'percentage') {
+                return bcsub($product->price, bcmul($product->price, $product->activeDiscount->discount_value / 100));
+            } elseif ($product->activeDiscount->discount_type === 'fixed') {
+                return bcsub($product->price, $product->activeDiscount->discount_value);
+            }
         }
-
+        // If no active discount, return the original price
         return $product->price;
     }
 }
