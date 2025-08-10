5589a0131c6431aff9ab567c06dfbbfb19aa92db
vicky wibisono <wibivicky@gmail.com>
2025-08-10 22:03:35 +0700

    update discount model

--- app/Models/Discount.php
+++ app/Models/Discount.php
@@ -8,11 +8,15 @@ class Discount extends Model
 {
     protected $fillable = [
         'code', 'discount_type', 'discount_amount',
+        'usage_limit', 'is_active',
         'minimum_order_total', 'start_at', 'end_at'
     ];
 
     protected $casts = [
         'start_at' => 'string',
         'end_at' => 'string',
+        'discount_amount' => 'string',
+        'minimum_order_total' => 'string',
+        'usage_limit' => 'integer',
     ];
 }
