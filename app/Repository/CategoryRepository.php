de566872893e872a13c00ff8aedf40d7ec20e6aa
vicky wibisono <wibivicky@gmail.com>
2025-08-15 23:54:29 +0700

    update category repository

--- app/Repository/CategoryRepository.php
+++ app/Repository/CategoryRepository.php
@@ -3,10 +3,12 @@
 namespace App\Repository;
 
 use App\Models\Category;
+use Illuminate\Support\Facades\Cache;
 
 class CategoryRepository
 {
     private $category;
+    private $cacheKey = 'categories';
 
     public function __construct(Category $category) {
         $this->category = $category;
@@ -14,22 +16,57 @@ class CategoryRepository
 
     public function get($params = [])
     {
-        $categories = $this->category
-            ->when(!empty($params['search']['name']), function ($query) use ($params) {
-                return $query->where('name', 'LIKE', '%' . $params['search']['name'] . '%');
-            });
+        $cacheKey = $this->generateCacheKey($params);
 
-        if (!empty($params['page'])) {
-            return $categories->paginate($params['page']);
-        }
+        return Cache::tags(['categories'])->rememberForever($cacheKey, function () use ($params) {
+            $categories = $this->category
+                ->when(!empty($params['search']['name']), function ($query) use ($params) {
+                    return $query->where('name', 'LIKE', '%' . $params['search']['name'] . '%');
+                });
 
-        return $categories->get();
+            if (!empty($params['page'])) {
+                return $categories->paginate($params['page']);
+            }
+
+            return $categories->get();
+        });
+    }
+
+    public function find($id)
+    {
+        return Cache::tags(['categories'])->rememberForever("category_{$id}", function () use ($id) {
+            return $this->category->find($id);
+        });
     }
 
     public function store(Category $category)
     {
         $category->save();
-
+        $this->clearCache();
         return $category;
+    }
+
+    public function update(Category $category)
+    {
+        $category->update();
+        $this->clearCache();
+        return $category;
+    }
+
+    public function delete(Category $category)
+    {
+        $category->delete();
+        $this->clearCache();
+        return $category;
+    }
+
+    private function generateCacheKey(array $params)
+    {
+        return $this->cacheKey . '_' . md5(json_encode($params));
+    }
+
+    public function clearCache()
+    {
+        Cache::forget($this->cacheKey);
     }
 }
