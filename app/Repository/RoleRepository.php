d7820ded903fda8ac73bac94b957637b305503e3
vicky wibisono <wibivicky@gmail.com>
2025-08-17 22:41:13 +0700

    add cache query on role repository

--- app/Repository/RoleRepository.php
+++ app/Repository/RoleRepository.php
@@ -3,10 +3,12 @@
 namespace App\Repository;
 
 use App\Models\Role;
+use Illuminate\Container\Attributes\Cache;
 
 class RoleRepository
 {
     private $role;
+    private $roleCache = 'roles';
 
     public function __construct(Role $role) {
         $this->role = $role;
@@ -14,11 +16,15 @@ class RoleRepository
 
     public function get($params = [])
     {
-        $roles = $this->role
-            ->when(!empty($params['search']['name']), function($query) use ($params) {
-                return $query->where('name', 'LIKE', '%' . $params['search']['name'] . '%');
-            });
-            
+        $cacheKey = $this->generateCacheKey($params);
+
+        return Cache::rememberForever($cacheKey, function() use ($params) {
+            $roles = $this->role
+                ->when(!empty($params['search']['name']), function($query) use ($params) {
+                    return $query->where('name', 'LIKE', '%' . $params['search']['name'] . '%');
+                });
+        });
+
         if (!empty($params['page'])) {
             return $roles->paginate($params['page']);
         }
@@ -29,7 +35,31 @@ class RoleRepository
     public function store(Role $role)
     {
         $role->save();
-
+        $this->clearCache();
         return $role;
     }
+
+    public function update(Role $role)
+    {
+        $role->update();
+        $this->clearCache();
+        return $role;
+    }
+
+    public function delete(Role $role)
+    {
+        $role->delete();
+        $this->clearCache();
+    }
+
+    private function generateCacheKey(array $params)
+    {
+        return $this->cacheKey . '_' . md5(json_encode($params));
+    }
+
+    public function clearCache()
+    {
+        Cache::forget($this->cacheKey);
+    }
+
 }
